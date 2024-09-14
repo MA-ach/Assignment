6d170829603f7e9d391aff1aacf9ee3cc4355976
Mannya <a1946814@adelaide.edu.au>
2024-09-14 20:17:40 +0930

    Add files via upload

---
+++ question02.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Initialize a new Git repository
+git init
+
+# Add all files in the current directory to the repository
+git add .
+
+# Commit the changes
+git commit -m "Initial commit"
+
